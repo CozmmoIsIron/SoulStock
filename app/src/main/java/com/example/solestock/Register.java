f192dcf6311e89ffd1ff344d070a9be2e9587051
CozmmoIsIron <vedantsnaidu@gmail.com>
2023-03-08 21:49:59 -0800

    Color blending bug fixes Intent Filter Changed Drawable error fixed

--- app/src/main/java/com/example/solestock/Register.java
+++ app/src/main/java/com/example/solestock/Register.java
@@ -3,12 +3,22 @@ package com.example.solestock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
-
+    EditText mUsername, mEmail, mPassword;
+    Button mSignupButton;
+    TextView mLoginTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmail= findViewById(R.id.emails);     // Initalizing views
+        mPassword= findViewById(R.id.password);
+        mLoginTxt= findViewById(R.id.logintext);
+        mSignupButton=findViewById(R.id.signup);
     }
 }
